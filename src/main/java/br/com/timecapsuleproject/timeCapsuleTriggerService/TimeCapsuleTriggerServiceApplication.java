c94ae85301677a2672a789773fd42e3986da305b
MarceloHofartK <hofartmarcelo@gmail.com>
2026-04-25 10:50:48 -0300

    Initial commit

---
+++ src/main/java/br/com/timecapsuleproject/timeCapsuleTriggerService/TimeCapsuleTriggerServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.timecapsuleproject.timeCapsuleTriggerService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeCapsuleTriggerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeCapsuleTriggerServiceApplication.class, args);
+	}
+
+}
